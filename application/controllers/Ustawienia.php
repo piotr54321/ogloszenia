0f24a43a1a02a69058f9c83919d654406538a497
piotr54321 <ziolko.piotr@outlook.com>
2019-11-09 14:28:23 +0100

    Ustawienia - Adresy: -podgląd -edycja Poprawki w innych plikach

--- application/controllers/Ustawienia.php
+++ application/controllers/Ustawienia.php
@@ -8,29 +8,156 @@
 
 class Ustawienia extends AC_Controller
 {
-    function __construct()
+	/**
+	 * @var array
+	 */
+	private $data;
+
+	function __construct()
     {
         parent::__construct();
         $this->load->library('twig');
+		$this->load->model('usersmodel');
+		$this->load->model('accessmodel');
+		$this->load->model('adressessmodel');
+		$this->load->library('form_validation');
+		$this->load->helper('security');
+		if(!isset($data)){
+			$data = [];
+		}
+		$this->data = array_merge($this->page_data(), $data, $this->session->flashdata());
     }
 
     function index(){
-		$data = $this->pageData();
-		$this->twig->display('settings/index.html', $data);
+
+		$this->twig->display('settings/index.html', $this->data);
     }
 
     function adresy(){
-		$data = $this->pageData();
-		$this->twig->display('settings/adresy.html', $data);
+
+
+		$this->data['user_adressess'] = $this->adressessmodel->adressessFind(['id_user' => $this->data['user']['id']]);
+		//var_dump($this->data['user_adressess']);
+		$this->twig->display('settings/adresy.html', $this->data);
     }
 
+    function adres_edytuj($address_id = null){
+		if(is_numeric($address_id)){
+			$this->data['address_edited']=$this->adressessmodel->adressessFind(['id_user' => $this->data['user']['id'], 'id_address' => $address_id])[0];
+		}
+		if (!is_numeric($address_id) || $this->data['address_edited'] == FALSE){
+			$this->data['error'] = 'Nieprawidłowe ID adresu';
+		}else{
+			//var_dump($this->data['address_edited']);
+			if($this->input->post()){
+				$this->form_validation->set_rules('city', 'city', 'trim|xss_clean');
+				$this->form_validation->set_rules('street', 'street', 'trim|xss_clean');
+				$this->form_validation->set_rules('home', 'home', 'trim|xss_clean');
+				$this->form_validation->set_rules('postal_code', 'postal_code', 'trim|xss_clean');
+				$this->form_validation->set_rules('main_address', 'main_address', 'trim|xss_clean');
+				$this->form_validation->set_rules('addressID', 'addressID', 'trim|xss_clean|required');
+
+				if($this->form_validation->run() == FALSE){
+					$this->session->set_flashdata('info', 'Wprowadzono niepoprawne dane');
+					redirect('/ustawienia/adres_edytuj/'.$address_id, 'location');
+				}else{
+					$data_address['update'] = [
+						'city' => $this->input->post('city'),
+						'street' => $this->input->post('street'),
+						'home' => $this->input->post('home'),
+						'postal_code' => $this->input->post('postal_code'),
+						'main_address' => $this->input->post('main_address'),
+					];
+					$data_address['where'] = [
+						'id_user' => $this->data['user']['id'],
+						'id_address' => $this->input->post('addressID')
+					];
+
+					if($this->adressessmodel->addressUpdate($data_address)){
+						$this->session->set_flashdata('complete', 'Poprawnie zmieniono dane adresu');
+						redirect('/ustawienia/adres_edytuj/'.$address_id, 'location');
+					}else{
+						$this->session->set_flashdata('error', 'Niepowodzenie');
+						redirect('/ustawienia/adres_edytuj/'.$address_id, 'location');
+					}
+				}
+			}
+		}
+
+		$this->twig->display('settings/adres_edytuj.html', $this->data);
+	}
+
+	function adres_dodaj(){
+
+		if($this->input->post()){
+			$this->form_validation->set_rules('city', 'city', 'trim|xss_clean|required');
+			$this->form_validation->set_rules('street', 'street', 'trim|xss_clean|required');
+			$this->form_validation->set_rules('home', 'home', 'trim|xss_clean|required');
+			$this->form_validation->set_rules('postal_code', 'postal_code', 'trim|xss_clean|required');
+			$this->form_validation->set_rules('main_address', 'main_address', 'trim|xss_clean|required');
+			if($this->form_validation->run() == FALSE){
+				$this->session->set_flashdata('info', 'Wprowadzono niepoprawne dane');
+				redirect('/ustawienia/adres_dodaj/', 'location');
+			}else{
+				$data_address['insert'] = [
+					'id_user' => $this->data['user']['id'],
+					'city' => $this->input->post('city'),
+					'street' => $this->input->post('street'),
+					'home' => $this->input->post('home'),
+					'postal_code' => $this->input->post('postal_code'),
+					'main_address' => $this->input->post('main_address'),
+				];
+
+				$addres_insert_status = $this->adressessmodel->addressInsert($data_address);
+				if($addres_insert_status){
+					$this->session->set_flashdata('complete', 'Poprawnie dodano adres');
+					redirect('/ustawienia/adres_edytuj/'.$addres_insert_status, 'location');
+				}else{
+					$this->session->set_flashdata('error', 'Niepowodzenie');
+					redirect('/ustawienia/adres_dodaj/', 'location');
+				}
+			}
+		}
+		$this->twig->display('settings/adres_dodaj.html', $this->data);
+	}
+
+	function adres_usun(){
+		if($this->input->post()){
+			$this->form_validation->set_rules('addressID', 'addressID', 'trim|xss_clean|required');
+			if($this->form_validation->run() == FALSE){
+				$this->session->set_flashdata('info', 'Wprowadzono niepoprawne dane');
+				redirect('/ustawienia/adres_edytuj/', 'location');
+			}else{
+				$data_address['delete'] = [
+					'id_address' => $this->input->post('addressID'),
+				];
+				$data_address['where'] = [
+				'id_user' => $this->data['user']['id']
+				];
+				if($this->adressessmodel->addressDelete($data_address)){
+					$this->session->set_flashdata('complete', 'Poprawnie usunięto adres');
+					redirect('/ustawienia/adresy/', 'location');
+				}else{
+					$this->session->set_flashdata('error', 'Niepowodzenie podczas usuwania adresu');
+					redirect('/ustawienia/adres_edytuj/'.$data_address['delete']['id_address'], 'location');
+				}
+			}
+		}
+	}
+
     function logowanie(){
-		$data = $this->pageData();
-		$this->twig->display('settings/logowanie.html', $data);
+		$this->data['findUser'] = $this->usersmodel->getUsers(['id' => $this->data['user']['id']])[0];
+
+
+		if($this->input->post()){
+
+		}
+
+		$this->twig->display('settings/logowanie.html', $this->data);
     }
 
     function daneosobowe(){
-		$data = $this->pageData();
-		$this->twig->display('settings/daneosobowe.html', $data);
+
+		$this->twig->display('settings/daneosobowe.html', $this->data);
     }
 }
---
+++ application/models/AdressessModel.php
@@ -0,0 +1,90 @@
+<?php
+
+class AdressessModel extends CI_Model{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->helper('dbHelp');
+	}
+
+	function adressessFind(array $dataFind){
+		if(!is_array($dataFind)){
+			return FALSE;
+		}
+		$this->db->select('id_address, id_user, main_address, postal_code, home, street, city');
+		$this->db->from('addresses');
+		issetWhere($this->db, $dataFind, 'id_address');
+		issetWhere($this->db, $dataFind, 'id_user');
+		issetWhere($this->db, $dataFind, 'main_address');
+		issetWhere($this->db, $dataFind, 'postal_code');
+		issetWhere($this->db, $dataFind, 'home');
+		issetWhere($this->db, $dataFind, 'street');
+		issetWhere($this->db, $dataFind, 'city');
+		$query = $this->db->get();
+
+		if($query->num_rows() > 0){
+			return $query->result_array();
+		}else{
+			return FALSE;
+		}
+	}
+
+	function addressUpdate(array  $dataUpdate){
+
+		$this->db->trans_start();
+		if($dataUpdate['update']['main_address'] == 1){
+			$this->addressUpdate(['update' => ['main_address' => 0], 'where' => ['id_user' => $dataUpdate['where']['id_user']]]);
+		}
+		issetSet($this->db, $dataUpdate['update'], 'city');
+		issetSet($this->db, $dataUpdate['update'], 'street');
+		issetSet($this->db, $dataUpdate['update'], 'home');
+		issetSet($this->db, $dataUpdate['update'], 'postal_code');
+		issetSet($this->db, $dataUpdate['update'], 'main_address');
+		issetWhere($this->db, $dataUpdate['where'], 'id_address');
+		issetWhere($this->db, $dataUpdate['where'], 'id_user');
+		$this->db->update('addresses');
+
+		$this->db->trans_complete();
+
+		if ($this->db->trans_status() === TRUE){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+
+	function addressInsert(array $dataInsert){
+		$this->db->trans_start();
+		if($dataInsert['insert']['main_address'] == 1){
+			$this->addressUpdate(['update' => ['main_address' => 0], 'where' => ['id_user' => $dataInsert['insert']['id_user']]]);
+		}
+		issetSet($this->db, $dataInsert['insert'], 'id_user');
+		issetSet($this->db, $dataInsert['insert'], 'city');
+		issetSet($this->db, $dataInsert['insert'], 'street');
+		issetSet($this->db, $dataInsert['insert'], 'home');
+		issetSet($this->db, $dataInsert['insert'], 'postal_code');
+		issetSet($this->db, $dataInsert['insert'], 'main_address');
+		$this->db->insert('addresses');
+		$insert_id = $this->db->insert_id();
+		$this->db->trans_complete();
+
+		if ($this->db->trans_status() === TRUE){
+			return $insert_id;
+		}else{
+			return FALSE;
+		}
+	}
+
+	function addressDelete(array $dataDelete){
+		issetWhere($this->db, $dataDelete['delete'], 'id_address');
+		issetWhere($this->db, $dataDelete['where'], 'id_user');
+		$this->db->delete('addresses');
+
+		if($this->db->affected_rows() > 0){
+			return TRUE;
+		}else{
+			return FALSE;
+		}
+	}
+}
